91134f83a7a6f8f88e50649addcac484c0f845d7
Sowmya <sowmya.aji@gmail.com>
2018-12-08 00:01:27 -0500

    Add ability for audio button to play url

--- main.js
+++ main.js
@@ -29,16 +29,31 @@ $('#search-button').on('click', function (event) {
 
 function tunesHtml(tunes) {
     return `
-     <p class="title"><a href="${tunes.previewUrl}">
-     
-     
-     ${tunes.trackName}</a></p><img src="${tunes.artworkUrl100}">
-    </p><p class"Album"> ${tunes.collectionName}</p><p class="singer"> ${tunes.artistName}</p> <p class="singersite">${tunes.artistViewUrl}</p> `
+     <p class="title"><a href="${tunes.previewUrl}" class="result-link">${tunes.trackName}</a></p><img src="${tunes.artworkUrl100}"></p>
+     <p class"Album"> ${tunes.collectionName}></p>
+     <p class="singer"> ${tunes.artistName}</p>
+    <p class="singersite">${tunes.artistViewUrl}></p> `
 }
 
-// document.getElementById('#musicbox').addEventListener("click", function (event) {
-//     if (event.target($) { ${ previewU } })
-// })          
+// $('#play').on('click', function (event) {
+//     console.log(event)
+//     $(event.currentTarget).attr("src", tunes.previewUrl);
+// })
+
+
+
+
+document.getElementById('musicbox').addEventListener("click", function (event) {
+    if (event.target.classList.contains('results-link')) {
+        console.log(event.target.href)
+        event.preventDefault()
+        var clickedItem = event.target.id;
+        document.getElementById('play').src = event.target.href;
+
+
+
+    }
+})
 
 
 
